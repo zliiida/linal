478b8a24e9ebde91d24aa3ce737afc3bf8cde8e5
zliiida <107354510+zliiida@users.noreply.github.com>
2022-06-30 21:13:49 +1000

    Add files via upload

---
+++ проект арк.pas
@@ -0,0 +1,41 @@
+﻿var act:integer;
+begin
+  writeln();
+  writeln('Чтобы посчитать определитель в матрице, тыкни 1');
+  writeln('Чтобы сложить две матрицы, тыкни 2');
+  writeln('Чтобы перемножить две матрицы, тыкни 3');
+  writeln('Чтобы найти решение СЛАУ методом Гаусса, тыкни 4');
+  writeln('Чтобы найти решение СЛАУ методом Крамера, тыкни 5');
+  writeln('Чтобы найти обратную матрицу, тыкни 6');
+  readln(act);
+  case act of
+  1: begin
+       
+     end;
+  2: begin
+       
+     end;
+  3: begin
+       
+     end;
+  4: begin
+       
+     end;
+  5: begin
+       
+     end;
+  6: begin
+       
+     end;
+  7: begin
+       
+     end;
+  8: begin
+       
+     end;
+  9: begin
+       
+     end;
+  end;
+end.
+
